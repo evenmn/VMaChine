653f9f0c85090b8fccf8b55795d3a15deea7533c
evenmn <evenmn@fys.uio.no>
2019-02-22 17:29:15 +0100

    Fixed brute-force Metropolis sampling

--- doc/src/Metropolis/bruteforce.cpp
+++ doc/src/Metropolis/bruteforce.cpp
@@ -12,11 +12,12 @@ BruteForce::BruteForce(System* system) :
 }
 
 bool BruteForce::acceptMove() {
-    m_positions     = m_system->getParticles();
-    double psiOld   = m_system->evaluateWaveFunctionSqrd();
-    int pRand       = m_system->getRandomNumberGenerator()->nextInt(m_numberOfFreeDimensions);
+    m_positions         = m_system->getParticles();
+    double psiOld       = m_system->evaluateWaveFunctionSqrd();
+    int pRand = m_system->getRandomNumberGenerator()->nextInt(m_numberOfFreeDimensions);
 
-    m_positions(pRand) += (m_system->getRandomNumberGenerator()->nextDouble() - 0.5) * m_stepLength;
+    Eigen::VectorXd oldPositions      = m_positions;
+    m_positions(pRand) = oldPositions(pRand) + 10 * (m_system->getRandomNumberGenerator()->nextDouble() - 0.5) * m_stepLength;
 
     m_system->updateAllArrays(m_positions, pRand);
     double psiNew = m_system->evaluateWaveFunctionSqrd();
@@ -24,6 +25,7 @@ bool BruteForce::acceptMove() {
     double w = psiNew/psiOld;
     double r = m_system->getRandomNumberGenerator()->nextDouble();
     if(w < r) {
+        m_positions(pRand) = oldPositions(pRand);
         m_system->resetAllArrays();
         return false;
     }
