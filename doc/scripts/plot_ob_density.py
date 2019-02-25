55396ff4596038d375ba2dc43e413b6abd1d44d1
evenmn <evenmn@fys.uio.no>
2019-02-25 15:43:44 +0100

    Added modified scripts and resampling tools written in C++

--- doc/scripts/plot_ob_density.py
+++ doc/scripts/plot_ob_density.py
@@ -9,25 +9,23 @@ def exact(r1):
 data1 = np.loadtxt("../data/OB.dat")
 #data2 = np.loadtxt("../data/OB_w_interaction.dat")
 
+data1 = data1[9500:]
+
 label_size = {"size":"14"}
 
-
 r1 = np.linspace(0, 5, len(data1))
-#r2 = np.linspace(0, 3, len(data2))
 
 data2 = exact(r1)
 data1 = data1/np.sum(data1)
-data2 = data2/np.sum(data2)
+data2 = 1.65*data2/np.sum(data2)
 
-sns.set()
 data1/=(r1)
-plt.plot(r1, data1, '.', markersize=4, label="Interaction")
-#plt.plot(r1, data2)
-
-#plt.plot(r2, data2, '-', linewidth=1.0, label="W")
-plt.title("6 particles with interaction")
+plt.plot(r1, data1, '.', markersize=2, label="Interaction")
+plt.plot(r1, data2, '--', linewidth=1.0, label="W")
+plt.title("2 particles with interaction")
 plt.xlabel("r", **label_size)
 plt.ylabel(r"$\rho$", **label_size)
 #plt.legend(loc="best")
-plt.savefig("../html/ob.png")
+plt.grid()
+plt.savefig("../html/ob_2^22.png")
 plt.show()
