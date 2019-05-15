df9948b3f1c3c86251c9e8b2887b78a663dc9c87
evenmn <evenmn@fys.uio.no>
2019-05-15 17:58:18 +0200

    Got 72 particle results

--- scripts/plot_tb_density.py
+++ scripts/plot_tb_density.py
@@ -3,65 +3,35 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 
-# Load file
-fileNames = ["../data/int1/twobody/VMC/2D/2P/0.100000w/ADAM_MC1048576.dat",
-             "../data/int1/twobody/VMC/2D/2P/0.280000w/ADAM_MC1048576.dat",
-             "../data/int1/twobody/VMC/2D/2P/0.500000w/ADAM_MC1048576.dat",
-             "../data/int1/twobody/VMC/2D/2P/1.000000w/ADAM_MC1048576.dat",
-             
-             "../data/int1/twobody/VMC/2D/6P/0.100000w/ADAM_MC1048576.dat",
-             "../data/int1/twobody/VMC/2D/6P/0.280000w/ADAM_MC1048576.dat",
-             "../data/int1/twobody/VMC/2D/6P/0.500000w/ADAM_MC1048576.dat",
-             "../data/int1/twobody/VMC/2D/6P/1.000000w/ADAM_MC1048576.dat",
-             
-             "../data/int1/twobody/VMC/2D/12P/0.100000w/ADAM_MC1048576.dat",
-             #"../data/int1/twobody/VMC/2D/12P/0.280000w/ADAM_MC1048576.dat",
-             #"../data/int1/twobody/VMC/2D/12P/0.500000w/ADAM_MC1048576.dat",
-             "../data/int1/twobody/VMC/2D/12P/1.000000w/ADAM_MC1048576.dat",
-             
-             "../data/int1/twobody/VMC/2D/20P/0.100000w/ADAM_MC1048576.dat",
-             #"../data/int1/twobody/VMC/2D/20P/0.280000w/ADAM_MC1048576.dat",
-             #"../data/int1/twobody/VMC/2D/20P/0.500000w/ADAM_MC1048576.dat",
-             #"../data/int1/twobody/VMC/2D/20P/1.000000w/ADAM_MC1048576.dat",
-             
-             "../data/int1/twobody/VMC/2D/30P/0.100000w/ADAM_MC1048576.dat",
-             "../data/int1/twobody/VMC/2D/30P/0.280000w/ADAM_MC1048576.dat",
-             "../data/int1/twobody/VMC/2D/30P/0.500000w/ADAM_MC1048576.dat",
-             "../data/int1/twobody/VMC/2D/30P/1.000000w/ADAM_MC1048576.dat",
-             
-             "../data/int1/twobody/VMC/2D/42P/0.100000w/ADAM_MC1048576.dat",
-             "../data/int1/twobody/VMC/2D/42P/0.280000w/ADAM_MC1048576.dat",
-             #"../data/int1/twobody/VMC/2D/42P/0.500000w/ADAM_MC1048576.dat",
-             "../data/int1/twobody/VMC/2D/42P/1.000000w/ADAM_MC1048576.dat",
-             
-             "../data/int1/twobody/VMC/2D/56P/0.100000w/ADAM_MC1048576.dat",
-             "../data/int1/twobody/VMC/2D/56P/0.280000w/ADAM_MC1048576.dat",
-             "../data/int1/twobody/VMC/2D/56P/0.500000w/ADAM_MC1048576.dat",
-             "../data/int1/twobody/VMC/2D/56P/1.000000w/ADAM_MC1048576.dat",
-            ]
+
+def generateFileName(method, dim, particle, omega):
+    fileName  = "../data/int1/twobody/"
+    fileName += method   + "/"
+    fileName += dim      + "D/"
+    fileName += particle + "P/"
+    fileName += omega    + "w/"
+    fileName += "ADAM_MC1048576.dat"
+    return fileName
 
 
-maxRadius = [10, 10, 10, 10,
-             15, 15, 15, 15,
-             25, 25,
-             30,
-             35, 35, 35, 35,
-             40, 40, 40,
-             45, 45, 45, 45,
-             50, 50, 50, 50,
-            ]
+def saveFigure(method, dim, particle, omega):
+    fileName  = "../plots/int1/twobody/" 
+    fileName += dim      + "D/" 
+    fileName += particle + "P/"
+    fileName += omega    + "w/"
+    fileName += method   + "_ADAM_MC2pow28.png"
+    plt.savefig(fileName)
+    
 
-
-for i in range(len(fileNames)):
-    print(i)
-    data = np.loadtxt(fileNames[i])
-
-    # Define parameters
-    numberOfDimensions = 2
+def crop(data, maxRadius, newRadius):
+    length = len(data)
+    newLength = int(length * (newRadius / maxRadius))
+    newData = data[0:newLength, 0:newLength]
+    return newData
+    
+    
+def norm(data, numberOfDimensions):
     numBins = len(data)
-
-    r = np.linspace(0, maxRadius[i], numBins)
-
     r_mat = np.zeros((numBins, numBins))
     for j in range(numBins):
         for k in range(numBins):
@@ -69,10 +39,41 @@ for i in range(len(fileNames)):
 
     data /= r_mat
     data /= np.sum(np.nan_to_num(data))
-
+    return data
+    
+    
+def plot(data, radius):
     plt.figure()
-    plt.imshow(data, cmap=plt.cm.jet, extent=[0,maxRadius[i],maxRadius[i],0])
+    #sns.heatmap(data, cmap="YlGnBu")
+    plt.imshow(data, cmap=plt.cm.jet, extent=[0,radius,radius,0])
     plt.colorbar()
     plt.xlabel("Radial distance from particle 1")
     plt.ylabel("Radial distance from particle 2")
-plt.show()
+
+
+def main():
+    maxRadius = [10,10,10,10]
+    newRadius = [8, 3.5, 3.5, 2.5]
+    
+    methods   = ['VMC']
+    dims      = ['2']
+    particles = ['2', '6']
+    omegas    = ['0.100000', '0.280000', '0.500000', '1.000000']
+    
+    for method in methods:
+        for dim in dims:
+            for particle in particles:
+                i = 0
+                for omega in omegas:
+                    fileName = generateFileName(method, dim, particle, omega)
+                    data = np.loadtxt(fileName)
+                    data = crop(data, maxRadius[i], newRadius[i])
+                    data = norm(data, int(dim))
+                    plot(data, newRadius[i])
+                    saveFigure(method, dim, particle, omega)
+                    i += 1
+    plt.show()
+        
+
+if __name__ == '__main__':
+    main()
