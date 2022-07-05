e71dfea46ec2c155bffaa35b467be182fec4f9a7
evenmn <evenmn@mn.uio.no>
2022-07-05 11:38:40 +0200

    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,13 @@
-#include <memory>
-
-#include "main.h"
+#include "system.h"
 
 int main(int argc, char **argv)
 {
     // Define system
     System base;
 
-    //base.setNumberOfParticles(2);
-    //base.setNumberOfDimensions(2);
-    //base.setEquilibrationFraction(0.001);
+    base.setNumberOfParticles(2);
+    base.setNumberOfDimensions(2);
+    base.setEquilibrationFraction(0.001);
 
     base.initializeFromConfig(argc, argv);
     base.runSimulation();
