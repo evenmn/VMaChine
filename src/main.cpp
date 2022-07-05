10a424b2e4a4e949202d93c602dc7d6cfd661585
evenmn <evenmn@mn.uio.no>
2022-07-05 09:21:46 +0200

    Updated main.cpp with system object no longer declared as a pointer

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
+#include <memory>
+
 #include "main.h"
 
 int main(int argc, char **argv)
 {
     // Define system
-    System *base = new System();
+    System base;
 
-    base->setNumberOfParticles(2);
-    base->setNumberOfDimensions(2);
-    base->setEquilibrationFraction(0.001);
+    //base.setNumberOfParticles(2);
+    //base.setNumberOfDimensions(2);
+    //base.setEquilibrationFraction(0.001);
 
-    base->initializeFromConfig(argc, argv);
-    base->runSimulation();
+    base.initializeFromConfig(argc, argv);
+    base.runSimulation();
+
     return 0;
 }
