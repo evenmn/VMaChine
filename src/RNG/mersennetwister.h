edb356eefb53d920f3ef84f8e4eeb33566567cce
evenmn <evenmn@mn.uio.no>
2022-07-26 13:55:11 +0200

    Range of uniform distribution can now be changed (pushed from my iPhone)

--- src/RNG/mersennetwister.h
+++ src/RNG/mersennetwister.h
@@ -10,10 +10,10 @@ public:
     MersenneTwister();
     void setSeed(int seed) override;
     int nextInt(int upperLimit) override;
-    double nextDouble() override;
+    double nextDouble(double = 0.0, double = 1.0) override;
     double nextGaussian(double mean, double variance) override;
     std::string getLabel() override { return m_label; }
-    Eigen::MatrixXd randomUniformMatrix(Eigen::Index row, Eigen::Index col);
+    Eigen::MatrixXd randomUniformMatrix(Eigen::Index row, Eigen::Index col, double = 0.0, double = 1.0);
     Eigen::MatrixXd randomNormalMatrix(Eigen::Index row, Eigen::Index col, double mean, double variance);
 private:
     std::mt19937 generator;
