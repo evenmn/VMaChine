79350a1548601521aeb981c34cc6a4db535caf65
evenmn <evenmn@mn.uio.no>
2022-08-12 10:18:42 +0200

    Name change

--- src/InitialWeights/xavier.cpp
+++ src/InitialWeights/xavier.cpp
@@ -16,6 +16,9 @@ void Javier::setupInitialWeights()
     low = -1./std::sqrt(degreesOfFreedom);
     high = 1./std::sqrt(degreesOfFreedom);
     m_parameters = m_system->getRandomNumberGenerator()->randomUniformMatrix(m_numberOfElements, m_maxParameters, low, high);
+    if (m_numberOfElements > 3) {
+	m_parameters.row(3) = m_system->getRandomNumberGenerator()->randomUniformMatrix(1, m_maxParameters, 0.0, 0.2);
+    }
     m_system->updateAllParameters(m_parameters);
 }
 
